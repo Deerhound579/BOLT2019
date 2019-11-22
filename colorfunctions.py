a8a74d7d0b7bbeac7962a93f9ac2c586ec91c3d6
Jiahao <jahowchen@outlook.com>
2019-11-22 13:06:11 -0500

    Update colorfunctions.py

--- colorfunctions.py
+++ colorfunctions.py
@@ -1,9 +1,14 @@
 from colour import Color
 
-
-def create_hue(tags: list): # a list of tags by store
+def tagToColor(tags: list): #a list of tags by store
+    colorDict = {"Food and Dining":"red",
+    "Shopping":"blue", 
+    "Entertainment":"purple",
+    "Auto and Transport":"green"}
+    return list(map (lambda x : colorDict[x], tags))
+def create_hue(colours: list): # a list of colors by store
     hueDict = {"red":0, "blue":(217/360),"green":(117/360),"purple":(291/360)}
-    return list(map (lambda x : hueDict[x], tags))
+    return list(map (lambda x : hueDict[x], colours))
 def create_lumin(freq: list): # a list of freq by store
     minv, maxv = min(freq), max(freq)
     diff = maxv-minv
@@ -16,8 +21,8 @@ def create_color(hue:list, lum:list): # return a list of colors in hex
         colors.append(Color(hsl=(h, sat, l)))
     return list(map(lambda c: c.hex, colors))
 
-tags = ["red", "red", "green", "blue", "blue"] # get tag list
-freqs = [1, 5, 10, 20, 50] # get freq list
+tags = ["Food and Dining", "Shopping", "Entertainment", "Auto and Transport"] # get tag list
+freqs = [20, 5, 10, 20, 50] # get freq list
 
-colors = create_color(create_hue(tags), create_lumin(freqs))
+colors = create_color(create_hue(tagToColor(tags)), create_lumin(freqs))
 print(colors)
